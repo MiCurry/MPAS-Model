1bd159597a7ec089127cfb025a03472e43083731
Miles A Curry <currymiles@gmail.com>
2019-08-21 16:07:01 +0000

    Add CPF Code gen options to build_options.mk
    This commit adds an environment variable to build_options.mk within
    src/core_atmosphere that will be exported to pass an option to the registry
    parser to turn run the code generation for MPAS Atmosphere.

--- src/core_atmosphere/build_options.mk
+++ src/core_atmosphere/build_options.mk
@@ -3,5 +3,8 @@ EXE_NAME=atmosphere_model
 NAMELIST_SUFFIX=atmosphere
 override CPPFLAGS += -DCORE_ATMOSPHERE
 
+CODE_GEN_OPTS=--cpf-module
+export CODE_GEN_OPTS
+
 report_builds:
 	@echo "CORE=atmosphere"
